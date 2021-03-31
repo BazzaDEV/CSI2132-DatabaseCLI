3f85c8ced828e36f0d38013b9a160ee3929882cd
Marija Bolic <mboli027@uottawa.ca>
2021-03-31 00:25:31 -0400

    Improved customer UI

--- src/main/java/cli/customer/CustomerMainMenu.java
+++ src/main/java/cli/customer/CustomerMainMenu.java
@@ -1,5 +1,6 @@
 package cli.customer;
 
+
 import cli.Menu;
 
 import java.text.ParseException;
@@ -14,6 +15,17 @@ import utils.Vars;
 public class CustomerMainMenu extends Menu {
 
     private Customer c;
+    private String VAR_START_DATE;
+    private String VAR_END_DATE;
+    private String VAR_CITY;
+    private String VAR_STATE;
+    private float VAR_MIN_PRICE;
+    private float VAR_MAX_PRICE;
+    private String VAR_VIEW;
+    private String VAR_ROOM_CAPACITY;
+    private Integer VAR_NUM_OCCUPANTS;
+    private String VAR_IS_EXTENDABLE;
+    private String VAR_AMENITIES;
 
     @Override
     public void start() {
@@ -23,7 +35,6 @@ public class CustomerMainMenu extends Menu {
         Helper.println("\n" + Vars.DIVIDER_EQUALS +
                         "\n" + StringUtils.center("Customer Main Menu", Vars.DIVIDER_EQUALS.length()) +
                         "\n" + Vars.DIVIDER_EQUALS);
-
         Helper.println("\nWelcome back, " + c.getName().getFirstName() + "!");
 
         Helper.println("\nCustomer info:\n\n" +
@@ -34,10 +45,10 @@ public class CustomerMainMenu extends Menu {
         boolean F6 = false;
         while (!F6) {
 
-            String start_date = Helper.getInput("What day would you like to check in? (yyyy-mm-dd)\n");
-            String end_date = Helper.getInput("What day would you like to check out? (yyyy-mm-dd)\n");
+            VAR_START_DATE  = Helper.getInput("What day would you like to check in? (yyyy-mm-dd)\n");
+            VAR_END_DATE  = Helper.getInput("What day would you like to check out? (yyyy-mm-dd)\n");
 
-            if (start_date.matches("\\d{4}-\\d{2}-\\d{2}") && end_date.matches("\\d{4}-\\d{2}-\\d{2}")) { 
+            if (VAR_START_DATE.matches("\\d{4}-\\d{2}-\\d{2}") && VAR_END_DATE.matches("\\d{4}-\\d{2}-\\d{2}")) { 
 
                 F6 = true;
 
@@ -53,8 +64,8 @@ public class CustomerMainMenu extends Menu {
         
         while (!F4) {
 
-            String VAR_CITY = Helper.getInput("City: \n");
-            String VAR_STATE = Helper.getInput("State: \n");
+            VAR_CITY = Helper.getInput("City: \n");
+            VAR_STATE = Helper.getInput("State: \n");
 
             if (VAR_CITY.matches("[a-zA-Z]+") && VAR_STATE.matches("[a-zA-Z]+")) { 
 
@@ -75,22 +86,18 @@ public class CustomerMainMenu extends Menu {
         
         while (!F5) {
 
-            String VAR_MIN_PRICE = Helper.getInput("Minimum price: \n");
-            String VAR_MAX_PRICE = Helper.getInput("Maximum price: \n");
+            String min = Helper.getInput("Minimum price: \n");
+            String max  = Helper.getInput("Maximum price: \n");
 
-            if (VAR_MIN_PRICE.equalsIgnoreCase("SKIP")) { 
-            	VAR_MIN_PRICE = null;
+            if (min.matches("[0-9]+") && max.matches("[0-9]+")) { 
+            	float VAR_MIN_PRICE=Float.parseFloat(min);  
+            	float VAR_MAX_PRICE=Float.parseFloat(max);  
+            	if (VAR_MIN_PRICE < VAR_MAX_PRICE) {
+                    F5 = true;
+            	}else { // Invalid entry
+                    Helper.println("The minimum must be less than maximum");
 
-            } 
-            if (VAR_MAX_PRICE.equalsIgnoreCase("SKIP")) { 
-            	VAR_MAX_PRICE = null;
-            	F5 = true;
-
-            } 
-            if (VAR_MIN_PRICE.matches("[0-9]+") && VAR_MAX_PRICE.matches("[0-9]+")) { 
-
-                F5 = true;
-                
+                }
 
             } else { // Invalid entry
                 Helper.println("Please enter a valid price range");
@@ -104,7 +111,7 @@ public class CustomerMainMenu extends Menu {
         while (!F1) {
         	
         	String[] view_options = new String[] {"sea", "mountain"};
-        	String VAR_VIEW = Helper.getInput("Would you like a (sea) or (mountain) view? \n");
+        	VAR_VIEW = Helper.getInput("Would you like a (sea) or (mountain) view? \n");
 
             if (Helper.multiCheck(VAR_VIEW, view_options)) { 
 
@@ -121,10 +128,17 @@ public class CustomerMainMenu extends Menu {
         while (!F2) {
         	
         	String[] room_options = new String[] {"single", "double"};
-            String VAR_ROOM_CAPACITY = Helper.getInput("Would you like a (single) or (double) room? \n");
+            VAR_ROOM_CAPACITY = Helper.getInput("Would you like a (single) or (double) room? \n");
 
             if (Helper.multiCheck(VAR_ROOM_CAPACITY, room_options)) { 
+            	
+                if (VAR_ROOM_CAPACITY.equalsIgnoreCase("single")) {
+                	VAR_NUM_OCCUPANTS = 1;
 
+                } else if (VAR_ROOM_CAPACITY.equalsIgnoreCase("double")) { // Employee login
+                	VAR_NUM_OCCUPANTS = 2;
+
+                }
                 F2 = true;
 
             } else { // Invalid entry
@@ -140,7 +154,7 @@ public class CustomerMainMenu extends Menu {
         while (!F3) {
         	
         	String[] extendable_options = new String[] {"y", "n"};
-            String VAR_IS_EXTENDABLE = Helper.getInput("(y)es or (n)o: \n");
+            VAR_IS_EXTENDABLE = Helper.getInput("(y)es or (n)o: \n");
 
             if (Helper.multiCheck(VAR_IS_EXTENDABLE, extendable_options)) { 
 
@@ -156,6 +170,22 @@ public class CustomerMainMenu extends Menu {
 
             } else { // Invalid entry
                 Helper.println("Please type either y or n.");
+
+            }
+        }
+        
+        boolean F7 = false;
+        while (!F7) {
+        	
+        	String[] ameneties_options = new String[] {"AC", "TV", "fridge"};
+        	VAR_AMENITIES = Helper.getInput("Are you looking for air conditioner(AC), television(TV) or a (fridge)? \n");
+
+            if (Helper.multiCheck(VAR_AMENITIES, ameneties_options)) { 
+
+                F7 = true;
+
+            } else { // Invalid entry
+                Helper.println("Please choose one of the provided options.");
 
             }
         }
