87387a1b15e2388de01deb1e5e6ca7fe902bcd0f
kbaza065 <bazarjani.kian@gmail.com>
2021-04-01 12:51:34 -0400

    Updated User classes

--- src/main/java/users/Admin.java
+++ src/main/java/users/Admin.java
@@ -5,10 +5,14 @@ import structs.Name;
 
 public class Admin extends Employee {
 
-    public static final String ROLE_NAME = "Database Administrator";
+    public static final String ROLE_NAME = "DB Administrator";
 
     public Admin(int sinNumber, Name name, Address address, int salary) {
         super(sinNumber, name, address, salary, ROLE_NAME);
+    }
+
+    public Admin(Employee e) {
+        super(e.sinNumber, e.name, e.address, e.getSalary(), ROLE_NAME);
     }
 
 }
