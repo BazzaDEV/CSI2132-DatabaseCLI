65cf4a2b91de347f55b51b7a26796cb5cc2d0f7c
Marija Bolic <mboli027@uottawa.ca>
2021-03-31 23:49:22 -0400

    Added customer functionality to be able to search for rooms

--- src/main/java/cli/customer/CustomerMainMenu.java
+++ src/main/java/cli/customer/CustomerMainMenu.java
@@ -21,11 +21,17 @@ public class CustomerMainMenu extends Menu {
     private String VAR_STATE;
     private float VAR_MIN_PRICE;
     private float VAR_MAX_PRICE;
+    private String min;
+    private String max;
     private String VAR_VIEW;
     private String VAR_ROOM_CAPACITY;
     private Integer VAR_NUM_OCCUPANTS;
     private String VAR_IS_EXTENDABLE;
     private String VAR_AMENITIES;
+    private String ID;
+    private String number;
+    private Integer hotel_ID;
+    private Integer room_number;
 
     @Override
     public void start() {
@@ -82,16 +88,17 @@ public class CustomerMainMenu extends Menu {
         
         //choose price range
         boolean F5 = false;
-        Helper.println("Please enter the price range or SKIP");
+        Helper.println("Please enter the price range");
         
         while (!F5) {
 
-            String min = Helper.getInput("Minimum price: \n");
-            String max  = Helper.getInput("Maximum price: \n");
+            min = Helper.getInput("Minimum price: \n");
+            max  = Helper.getInput("Maximum price: \n");
 
             if (min.matches("[0-9]+") && max.matches("[0-9]+")) { 
             	float VAR_MIN_PRICE=Float.parseFloat(min);  
-            	float VAR_MAX_PRICE=Float.parseFloat(max);  
+            	float VAR_MAX_PRICE=Float.parseFloat(max); 
+            	
             	if (VAR_MIN_PRICE < VAR_MAX_PRICE) {
                     F5 = true;
             	}else { // Invalid entry
@@ -104,6 +111,9 @@ public class CustomerMainMenu extends Menu {
 
             }
         }
+        //need to convert the max and min to float outside of loop
+    	float VAR_MIN_PRICE=Float.parseFloat(min);  
+    	float VAR_MAX_PRICE=Float.parseFloat(max); 
 
         
         //choose sea or mountain
@@ -139,6 +149,7 @@ public class CustomerMainMenu extends Menu {
                 	VAR_NUM_OCCUPANTS = 2;
 
                 }
+                
                 F2 = true;
 
             } else { // Invalid entry
@@ -178,16 +189,55 @@ public class CustomerMainMenu extends Menu {
         while (!F7) {
         	
         	String[] ameneties_options = new String[] {"AC", "TV", "fridge"};
-        	VAR_AMENITIES = Helper.getInput("Are you looking for air conditioner(AC), television(TV) or a (fridge)? \n");
+        	VAR_AMENITIES = Helper.getInput("Are you looking for air conditioning(AC), television(TV) or a (fridge)? \n");
+            
 
             if (Helper.multiCheck(VAR_AMENITIES, ameneties_options)) { 
+            	
+            	if (VAR_AMENITIES.equalsIgnoreCase("AC")) {
+            		VAR_AMENITIES = "air conditioning";
 
+                } 
                 F7 = true;
 
             } else { // Invalid entry
                 Helper.println("Please choose one of the provided options.");
 
             }
+        }
+        
+        //displaying available hotel rooms
+        System.out.println("These are the available hotel rooms: \n");
+        boolean result = Customer.getRooms(VAR_START_DATE, VAR_END_DATE, VAR_CITY, VAR_STATE, VAR_MIN_PRICE, VAR_MAX_PRICE, VAR_VIEW, VAR_ROOM_CAPACITY, VAR_IS_EXTENDABLE, VAR_AMENITIES);
+    
+        if (result == false) {
+        	Helper.println("If you wish to book a hotel room please restart the process. Otherwise we wish you a great day!");
+        } else {
+            //user chooose room for booking
+            boolean Flag = false;
+            System.out.println("Please enter the hotel ID and room number of the room you wish to book");
+            
+            while (!Flag) {
+
+                ID = Helper.getInput("Hotel ID: \n");
+                number  = Helper.getInput("Room Number: \n");
+
+                if (min.matches("[0-9]+") && max.matches("[0-9]+")) { 
+                	
+                    Flag = true;
+
+                } else { // Invalid entry
+                    Helper.println("Please enter a valid hotel_ID and room_number");
+
+                }
+            }
+            //need to convert to integer
+        	int hotel_ID=Integer.parseInt(ID);  
+        	int room_number=Integer.parseInt(number); 
+        	
+        	//booking room
+        	//Customer.bookRooms(hotel_ID, room_number, VAR_ROOM_CAPACITY, VAR_NUM_OCCUPANTS, VAR_START_DATE, VAR_END_DATE);
+
         }
 
     }
