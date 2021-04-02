67b4d7adaa75e4800de574c3c4c357a4923aaea9
kbaza065 <bazarjani.kian@gmail.com>
2021-04-02 14:33:04 -0400

    Added CLI arguments to directly login

--- src/main/java/cli/CLIManager.java
+++ src/main/java/cli/CLIManager.java
@@ -1,8 +1,15 @@
 package cli;
 
+import cli.admin.AdminMainMenu;
+import cli.customer.CustomerMainMenu;
+import cli.employee.EmployeeMainMenu;
 import cli.login.LoginMenu;
 import database.SQLDatabaseConnection;
+import users.Admin;
+import users.Customer;
+import users.Employee;
 import users.User;
+import utils.Helper;
 
 import java.util.Date;
 import java.util.Stack;
@@ -29,6 +36,30 @@ public class CLIManager {
     public void startCLI() {
         LoginMenu loginMenu = new LoginMenu();
         loadMenu(loginMenu);
+    }
+
+    public void startCLI(String userType, String sinNumber) {
+        menuStack.add(new LoginMenu());
+
+        if (userType.equalsIgnoreCase("C")) { // Customer login
+            Customer c = Helper.getCustomerFromSIN(sinNumber);
+            setUser(c);
+            loadMenu(new CustomerMainMenu());
+        } else if (userType.equalsIgnoreCase("E")) { // Employee login
+            Employee e = Helper.getEmployeeFromSIN(sinNumber);
+            setUser(e);
+            loadMenu(new EmployeeMainMenu());
+
+        } else if (userType.equalsIgnoreCase("A")) { // Admin login
+            Employee e = Helper.getEmployeeFromSIN(sinNumber);
+            if (e != null && e.getRole().equalsIgnoreCase(Admin.ROLE_NAME)) { // Employee exists
+                Admin a = new Admin(e);
+                setUser(a);
+                loadMenu(new AdminMainMenu());
+
+            }
+
+        }
     }
 
     public void loadMenu(Menu menu) {
