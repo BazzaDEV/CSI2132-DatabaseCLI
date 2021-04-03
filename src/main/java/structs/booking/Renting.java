2a2bd5f91d66d05deae675dd75c8a3ac86056e84
kbaza065 <bazarjani.kian@gmail.com>
2021-04-03 13:07:33 -0400

    Commit changes to rebase 2

---
+++ src/main/java/structs/booking/Renting.java
@@ -0,0 +1,9 @@
+package structs.booking;
+
+public class Renting {
+
+    private int rentingID;
+    private String status;
+    private double balance;
+
+}
---
+++ src/main/java/structs/hotel/HotelRoom.java
@@ -0,0 +1,13 @@
+package structs.hotel;
+
+public class HotelRoom {
+
+    private int roomNumber;
+    private int hotelID;
+    private int price;
+    private int roomCapacity;
+    private String view;
+    private boolean isExtendable;
+    private String roomStatus;
+
+}
