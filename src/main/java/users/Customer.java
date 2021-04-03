49397919be48bd16a2f873e79932df94d43400b3
kbaza065 <bazarjani.kian@gmail.com>
2021-04-03 12:25:24 -0400

    Added getBookings() to return list of customer's bookings for a select date

--- src/main/java/users/Customer.java
+++ src/main/java/users/Customer.java
@@ -1,10 +1,18 @@
 package users;
 
+import database.SQLDatabaseConnection;
 import structs.Address;
 import structs.Name;
+import structs.booking.Booking;
+import sun.security.acl.AclImpl;
+import utils.Vars;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Customer extends User {
 
@@ -23,5 +31,47 @@ public class Customer extends User {
                 .append("Registration Date: ").append(new SimpleDateFormat("MMMMMMMM dd yyyy").format(registrationDate));
 
         return strB.toString().trim();
+    }
+
+    public List<Booking> getBookings(Date todaysDate) {
+
+        SQLDatabaseConnection db = SQLDatabaseConnection.getInstance();
+
+        String tDate = Vars.DATE_FORMAT.format(todaysDate);
+
+        try {
+            ResultSet rs = db.executeQuery("SELECT B.*" +
+                    " FROM Booking as B, CanCreate as CC" +
+                    " WHERE CC.sin_number = " + super.sinNumber +
+                    " AND CC.booking_ID = B.booking_ID" +
+                    " AND B.start_date = " + "'" + tDate + "'");
+
+            if (!rs.next()) { // Query has no results
+                return null;
+
+            } else {
+                List<Booking> bookingsList = new ArrayList<>();
+
+                do {
+                    int bookingID = rs.getInt("booking_ID");
+
+                    String status = rs.getString("status");
+                    String roomType = rs.getString("room_type");
+                    int numOccupants = rs.getInt("num_occupants");
+
+                    Date startDate = rs.getDate("start_date");
+                    Date endDate = rs.getDate("end_date");
+
+                    bookingsList.add(new Booking(bookingID, status, roomType, numOccupants, startDate, endDate));
+
+                } while(rs.next());
+
+                return bookingsList;
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return null;
+        }
     }
 }
