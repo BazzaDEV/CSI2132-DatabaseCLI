67895e7150e459ec60075f55b47217c9cb9fb94a
kbaza065 <bazarjani.kian@gmail.com>
2021-04-03 12:25:24 -0400

    Added getBookings() to return list of customer's bookings for a select date

--- src/main/java/users/Customer.java
+++ src/main/java/users/Customer.java
@@ -1,15 +1,23 @@
 package users;
 
+import database.SQLDatabaseConnection;
 import structs.Address;
 import structs.Name;
+import structs.booking.Booking;
+import sun.security.acl.AclImpl;
+import utils.Vars;
 import utils.Helper;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import database.SQLDatabaseConnection;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import database.SQLDatabaseConnection;
 
@@ -31,8 +39,8 @@ public class Customer extends User {
 
         return strB.toString().trim();
     }
-    
-    
+
+
     public static boolean getRooms(String VAR_START_DATE, String VAR_END_DATE, String VAR_CITY, String VAR_STATE, float VAR_MIN_PRICE, float VAR_MAX_PRICE, String VAR_VIEW, String VAR_ROOM_CAPACITY, String VAR_IS_EXTENDABLE, String VAR_AMENITIES) {
         SQLDatabaseConnection db = SQLDatabaseConnection.getInstance();
 
@@ -44,7 +52,7 @@ public class Customer extends User {
             		+ " FROM Hotel H, HotelRoom HR, BooksFor BF, Booking B"
             		+ " WHERE (H.city, H.state_name) = ('"+ VAR_CITY +"', '" +VAR_STATE+ "')"
             		+ " AND H.hotel_ID = HR.hotel_ID"
-            		+ " AND HR.price >= " + VAR_MIN_PRICE + " AND HR.price <= " + VAR_MAX_PRICE 
+            		+ " AND HR.price >= " + VAR_MIN_PRICE + " AND HR.price <= " + VAR_MAX_PRICE
             		+ " AND (HR.view, HR.room_capacity, HR.is_extendable) = ('"+ VAR_VIEW + "', '" + VAR_ROOM_CAPACITY + "', '" + VAR_IS_EXTENDABLE+"')"
             		+ " AND (HR.hotel_ID, HR.room_number) = (BF.hotel_ID, BF.room_number)"
             		+ " AND BF.booking_ID = B.booking_ID)"
@@ -69,54 +77,54 @@ public class Customer extends User {
             	System.out.println("There were no available rooms that met that criteria");
             	return false;
             } else {
-            		
+
                 do {
-                	
+
                 	//hotel room ID
                     String hotel_ID = rs.getString("hotel_ID");
                     System.out.println("Hotel ID: " + hotel_ID);
-                    
+
                     String room_number = rs.getString("room_number");
                     System.out.println("Room Number: " + room_number +"\n");
-                  
+
                     //user selected info
                     System.out.println("Selected criteria");
-                    
+
                     String price = rs.getString("price");
                     System.out.println("Price: " + price);
-                    
+
                     String view = rs.getString("view");
                     System.out.println("View: " + view);
-                    
+
                     String room_capacity = rs.getString("room_capacity");
                     System.out.println("Room Capacity: " + room_capacity);
-                    
+
                     String is_extendable = rs.getString("is_extendable");
                     System.out.println("Room is extendable: " + is_extendable);
-                    
+
                     String amenity = rs.getString("amenity");
                     System.out.println("Amenity: " + amenity + "\n");
-                    
+
                     //Hotel info
                     System.out.println("Hotel Information");
-                    
+
                     String star_category = rs.getString("star_category");
                     System.out.println("Star Category: " + star_category);
-                    
+
                     String city = rs.getString("city");
                     System.out.println("City: " + city);
-                    
+
                     String state_name = rs.getString("state_name");
                     System.out.println("State Name: " + state_name);
-                    
+
                     String zip = rs.getString("zip");
                     System.out.println("Zip: " + zip);
-                    
+
                     String email_address = rs.getString("email_address");
                     System.out.println("Email if you have any inquires: " + email_address + "\n\n");
 
-                    
-                    
+
+
                 } while (rs.next());
                 return true;
             }
@@ -129,9 +137,9 @@ public class Customer extends User {
 
         return false;
     }
-    
+
     public static void bookRooms(Integer hotel_ID, Integer room_number, String VAR_ROOM_CAPACITY, Integer VAR_NUM_OCCUPANTS, String VAR_START_DATE, String VAR_END_DATE) {
-    	
+
     	SQLDatabaseConnection db = SQLDatabaseConnection.getInstance();
 
         //execute the sql SELECT statement
@@ -139,7 +147,7 @@ public class Customer extends User {
         	//first insert into Booking
         	db.executeUpdate("INSERT into Booking(booking_ID, status, room_type, num_occupants, start_date, end_date)"
             		+ " VALUES(default, 'scheduled', '" +VAR_ROOM_CAPACITY+ "', '"+ VAR_NUM_OCCUPANTS+ "', '" +VAR_START_DATE+ "', '" +VAR_END_DATE+ "');");
-            
+
         	//need this select statement to get the booking ID we just inserted
         	ResultSet rs = db.executeQuery("SELECT max(booking_ID) as b_ID FROM Booking");
         	rs.next();
@@ -148,16 +156,58 @@ public class Customer extends User {
         	//Now insert into BooksFor
             db.executeUpdate(" Insert into BooksFor(booking_ID, room_number, hotel_ID)"
             		+ " values(" +b_ID+ ", "+ room_number+ ", " +hotel_ID+ ")");
-            		
 
-        	
+
+
         	Helper.println("The booking has been successfully created!");
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-    	
+
     }
-    
-    
+
+
+
+    public List<Booking> getBookings(Date todaysDate) {
+
+        SQLDatabaseConnection db = SQLDatabaseConnection.getInstance();
+
+        String tDate = Vars.DATE_FORMAT.format(todaysDate);
+
+        try {
+            ResultSet rs = db.executeQuery("SELECT B.*" +
+                    " FROM Booking as B, CanCreate as CC" +
+                    " WHERE CC.sin_number = " + super.sinNumber +
+                    " AND CC.booking_ID = B.booking_ID" +
+                    " AND B.start_date = " + "'" + tDate + "'");
+
+            if (!rs.next()) { // Query has no results
+                return null;
+
+            } else {
+                List<Booking> bookingsList = new ArrayList<>();
+
+                do {
+                    int bookingID = rs.getInt("booking_ID");
+
+                    String status = rs.getString("status");
+                    String roomType = rs.getString("room_type");
+                    int numOccupants = rs.getInt("num_occupants");
+
+                    Date startDate = rs.getDate("start_date");
+                    Date endDate = rs.getDate("end_date");
+
+                    bookingsList.add(new Booking(bookingID, status, roomType, numOccupants, startDate, endDate));
+
+                } while(rs.next());
+
+                return bookingsList;
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return null;
+        }
+    }
 }
