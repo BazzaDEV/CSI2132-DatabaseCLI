56e61a1a65aa6de584be1672d2d16e3954abda9a
Marija Bolic <mboli027@uottawa.ca>
2021-04-03 21:30:21 -0400

    Added update statement to update room to scheduled

--- src/main/java/users/Customer.java
+++ src/main/java/users/Customer.java
@@ -61,7 +61,7 @@ public class Customer extends User {
             		+ " AND (HR.hotel_ID, HR.room_number) = (BF.hotel_ID, BF.room_number)"
             		+ " AND BF.booking_ID = B.booking_ID)"
             		//main select statement
-            		+ " SELECT H.hotel_ID, HR.room_number, HR.price, HR.room_capacity, HR.view, HR.is_extendable, HA.amenity, H.star_category, H.city, H.state_name, H.zip, H.email_address"
+            		+ " SELECT DISTINCT H.hotel_ID, HR.room_number, HR.price, HR.room_capacity, HR.view, HR.is_extendable, HA.amenity, H.star_category, H.city, H.state_name, H.zip, H.email_address"
             		+ " FROM Hotel as H, HotelRoom as HR, BooksFor as BF, Booking as B, HotelRoomAmenities as HA"
             		+ " WHERE (H.city, H.state_name) = ('"+ VAR_CITY +"', '" +VAR_STATE+ "')"
             		+ " AND H.hotel_ID = HR.hotel_ID"
@@ -171,6 +171,11 @@ public class Customer extends User {
             //Insert into CanCreate
             db.executeUpdate(" Insert into CanCreate(booking_ID, sin_number)"
             		+ " values(" +b_ID+ ", "+sin+ ")");
+            
+            //Insert into CanCreate
+            db.executeUpdate(" Update HotelRoom SET room_status = 'scheduled'"
+            		+ " WHERE hotel_ID = "+hotel_ID+" AND room_number = "+room_number+";");            
+
 
         	Helper.println("The booking has been successfully created!");
 
